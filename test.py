cc0769d6eba742199b0a7b62327d04b4035db3f4
Jimmy James <jimmyjameslopez8@gmail.com>
2022-02-16 08:15:26 -0600

    Great...

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
 import time
+import json
+
 i=0
-while True:
-    print("Where on "+ str(i))
-    i = i+1
-    time.sleep(5)
+def main(environ,start_response):
+    while True:
+        headers = [('Content-type','text/html')]
+        print("Where on "+ str(i))
+        start_response('200',headers)
+        i = i+1
+        time.sleep(5)
+        response = {
+            "Result ==> ":"Nce"    
+        }
+        return [bytes(json.dumps(response),'utf-8')]
+
+
+if __name__ == "__main__":
+    main()
